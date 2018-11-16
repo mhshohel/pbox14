826df38d9bfc5bb46cbd294a9e406e9cbfd6e471
mhshohel <shohel@portfoliobox.net>
2018-11-16 22:27:05 +0100

    first commit

--- pull.php
+++ pull.php
@@ -1,22 +1,96 @@
 <?php
+session_start();
 
-//echo 'pull4';
+if(!isset($_SESSION['served']) || empty($_SESSION['served'])) {
+	$_SESSION['served'] = 0;
+}
 
-//echo shell_exec("/var/www/html/pbox14/pull/pull.sh");
+if(!isset($_SESSION['time']) || empty($_SESSION['time'])) {
+	$_SESSION['time'] = time();
+}
 
-//sudo /bin/bash /var/www/my_bash_script.sh
+if(!isset($_SESSION['isPulling']) || empty($_SESSION['isPulling'])) {
+	$_SESSION['isPulling'] = 0;
+}
 
-//$output = shell_exec("/var/www/html/pbox14/pull/pull.sh");
-//echo "<pre>$output</pre>";
-//
-//$output = shell_exec("cd /var/www/html/pbox14 && ls -la && sudo git pull");
-//echo "<pre>$output</pre>";
+if(!isset($_SESSION['pulledTime']) || empty($_SESSION['pulledTime'])) {
+	$_SESSION['pulledTime'] = 0;
+}
+
+$MAX_USE = 20; //Can use without auth
+$MAX_ACCESS_TIME_DIFF = 30; //after this seconds need auth
+$MAX_PULL_TIMEOUT = 10; //waiting time for next pull request
+
+$user = "admin";
+$pass = "admin";
+
+$_SESSION['served'] += 1;
+
+$differenceInSeconds = time() - $_SESSION['time'];
+
+if(isset($_SERVER['PHP_AUTH_USER']) && $_SERVER['PHP_AUTH_USER'] == $user && isset($_SERVER['PHP_AUTH_PW']) && $_SERVER['PHP_AUTH_PW'] == $pass) {
+	if($_SESSION['served'] > $MAX_USE || $differenceInSeconds > $MAX_ACCESS_TIME_DIFF){
+		$_SESSION['served'] = 0;
+		unset($_SESSION['time']);
 
 
-echo shell_exec("cd /var/www/html/pbox14/ && git pull 2>&1")
-//echo shell_exec("bash pull.sh 2>&1")
+		header('WWW-Authenticate: Basic realm="My Realm"');
+		header('HTTP/1.0 401 Unauthorized');
 
-//$output = shell_exec("ls -la /efs");
-//echo "<pre>$output</pre>";
+		echo "<pre>Unauthorized Access.</pre>";
+	}else{
+		$differenceInSecondsPull = 0;
+
+		if(isset($_SESSION['pulledTime']) && !empty($_SESSION['pulledTime'])){
+			$differenceInSecondsPull = time() - $_SESSION['pulledTime'];
+		}
+
+		if($_SESSION['isPulling'] == 0){
+			$_SESSION['isPulling'] = 1;
+
+			$_SESSION['pulledTime'] = time();
+
+
+			echo "<pre>-------------------------<br/>Executing Pull Request...<br/>-------------------------</pre>";
+
+			$output = shell_exec("cd /var/www/html/pbox14/ && git pull 2>&1");
+			echo "<pre>".$output."</pre>";
+
+			sleep(1);
+
+			echo "<pre>-------------------------<br/>Pulling completed...<br/>-------------------------</pre>";
+		}else {
+			if ($differenceInSecondsPull > $MAX_PULL_TIMEOUT) {
+				$_SESSION['isPulling'] = 0;
+				$_SESSION['pulledTime'] = 0;
+			}
+
+			$wait = $MAX_PULL_TIMEOUT - $differenceInSecondsPull;
+			$wait = ($wait <= 0) ? 1 : $wait;
+
+			echo "<pre>Cannot pull data, Please wait " . $wait . " seconds</pre>";
+		}
+	}
+} else {
+	header('WWW-Authenticate: Basic realm="My Realm"');
+	header('HTTP/1.0 401 Unauthorized');
+
+	echo "<pre>Unauthorized Access.</pre>";
+}
 
 ?>
+
+<style>
+	*{
+		margin-top: 50px;
+		background: #1b6b93;
+	}
+
+	pre{
+		text-align: center;
+		width: 100%;
+		font-size: 30px;
+		color: #ffffff;
+		font-weight: bold;
+	}
+</style>
